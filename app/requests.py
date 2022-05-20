f754d12ce74c5c80a55e85b7d08e80d65e4359b5
chepceline <chepceline25@gmail.com>
2022-05-21 01:08:13 +0300

    create requests

---
+++ app/requests.py
@@ -0,0 +1,7 @@
+import app.requests as requests,json
+
+def get_quotes():
+    response = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    if response.status_code == 200:
+        quote = response.json()
+        return quote
