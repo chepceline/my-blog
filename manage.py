d754e7e695ac5a2b6a7a6a1c086bba51168a6bec
celine <chepceline25@gmail.com>
2022-06-21 11:55:56 +0300

    add changes

--- manage.py
+++ manage.py
@@ -1,27 +1,19 @@
-from flask_script import Manager,Server
+from app import create_app, db
+from flask_script import Manager, Server
 from app.models import User
 from flask_migrate import Migrate, MigrateCommand
-from app import create_app,db
 
-app = create_app()
+app = create_app('development')
 
-manager =  Manager(app)
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
-manager.add_command('run',Server(use_debugger=True))
+manager = Manager(app)
+manager.add_command('server', Server)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
-
-@manager.command
-def test():
-    '''
-    Run the unit tests
-    '''
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=5).run(tests)
+    return dict(app = app, db = db, User = User)
 
 if __name__ == '__main__':
     manager.run()
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='aa;ag8CFuGqen;YpA}}-G%64C}ggiN'
 export MAIL_USERNAME='chepceline25@gmail.com'
 export MAIL_PASSWORD='Chepchirchir'
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://damaris:1234@localhost/power'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://celine:cel250@localhost/blog'
 
 python3.8 manage.py server
