087a89b5c2d17d30be4e36e9dfca06da8d9f02cb
celine <chepceline25@gmail.com>
2022-06-21 11:50:17 +0300

    add secret key

--- config.py
+++ config.py
@@ -1,14 +1,47 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS=True
-    MAIL_SERVER = 'smtp.gmail.com'
+    '''
+    General configuration parent class
+    '''
+    QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = SECRET_KEY = 'aa;ag8CFuGqen;YpA}}-G%64C}ggiN'
+    UPLOADED_PHOTOS_DEST = 'app/static/img'
+    
+    # Email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://celine:cel250@localhost/blog'
     DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
